b3862c1f755a735f0fbb7f027550dc34d91131dd
huamingyi <1442162538@qq.com>
2021-08-14 16:11:42 +0800

    首页完成

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+$(function () {
+    getUserInfo()
+    // 点击退出
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+
+            if (res.code !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            console.log(res);
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染头像
+function renderAvatar(user) {
+    // 获取用户的名称
+    var name = user.ninkname || user.username
+    // 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        //  渲染图片头像
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
+        $('.text-avatar').hide()
+    } else {
+        //  渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar')
+            .html(first)
+            .show()
+    }
+}
