d93e1962e6f163c6bab5e7782fd08df9ea4370a5
huamingyi <1442162538@qq.com>
2021-08-18 16:17:49 +0800

    文章管理完成

---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,106 @@
+
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    // 初始化富文本编辑器
+    initEditor()
+    initCate()
+    // 定义加载文章分类的方法 
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/cate/list',
+            success: function (res) {
+                if (res.code !== 0) {
+                    return layer.msg('初始化文章分类失败！')
+                }
+                // 调用模板引擎，渲染分类的下拉菜单  
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 一定要记得调用 form.render() 方法   
+                form.render()
+            }
+        })
+    };
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+    // 1.2 配置选项 
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域   
+        preview: '.img-preview'
+    }
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
+
+    // 为上传按钮绑定点击事件
+    $('#btnChooseImage').on('click', function () {
+        $('#cover_img').click()
+    });
+    // 为文件选择框绑定 change 事件
+    $('#cover_img').on('change', function (e) {
+        // 获取用户选择的文件
+        // console.log(e);
+        var filelist = e.target.files
+        if (filelist.length === 0) {
+            return layer.msg('请选择照片！')
+        }
+
+        // 1. 拿到用户选择的文件
+        var file = e.target.files[0]
+        // 2. 将文件，转化为路径
+        var imgURL = URL.createObjectURL(file)
+        // 3. 重新初始化裁剪区域
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', imgURL) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    });
+    var art_state = '已发布'
+    $('#btnSave2').on('click', function () {
+        art_state = '草稿'
+    });
+    $('#form-pub').on('submit', function (e) {
+        // 1. 阻止表单的默认提交行为
+        e.preventDefault()
+        // 2. 基于 form 表单，快速创建一个 FormData 对象
+        var fd = new FormData($(this)[0])
+        // 3. 将文章的发布状态，存到 fd 中
+        fd.append('state', art_state)
+        // 4. 将封面裁剪过后的图片，输出为一个文件对象
+        $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 400,
+                height: 280
+            })
+            .toBlob(function (blob) {
+                // 将 Canvas 画布上的内容，转化为文件对象
+                // 得到文件对象后，进行后续的操作
+                // 5. 将文件对象，存储到 fd 中
+                fd.append('cover_img', blob)
+                // 6. 发起 ajax 数据请求
+                publishArticle(fd)
+            })
+    });
+    function publishArticle(fd) {
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/add',
+            data: fd,
+            // 注意：如果向服务器提交的是 FormData 格式的数据，
+            // 必须添加以下两个配置项
+            contentType: false,
+            processData: false,
+            success: function (res) {
+                if (res.code !== 0) {
+                    return layer.msg('发布文章失败！')
+                }
+                layer.msg('发布文章成功！')
+                // 发布文章成功后，跳转到文章列表页面
+                location.href = '/article/art_list.html'
+            }
+        })
+    }
+})
